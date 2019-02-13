3cb56a1c2f2094d8e87056a2e316cedbe37ff821
Naiba <1@5.nu>
2019-02-13 17:51:33 +0800

    dep init

--- app/web/main.go
+++ app/web/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello")
+	r := gin.Default()
+	r.LoadHTMLGlob("resource/template/**/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "", gin.H{})
+	})
+	r.Run()
 }
