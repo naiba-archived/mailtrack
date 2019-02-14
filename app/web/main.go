f6233b589feeffd3ff6c2e1b776f339280d82b52
Naiba <1@5.nu>
2019-02-14 14:32:39 +0800

    I18n

--- app/web/main.go
+++ app/web/main.go
@@ -1,17 +1,55 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/locales/en"
+	"github.com/go-playground/locales/zh_Hans_CN"
+	"github.com/go-playground/universal-translator"
+	"github.com/naiba/mailtrack"
 )
 
 func main() {
+	zh := zh_Hans_CN.New()
+	utrans := ut.New(zh, zh, en.New())
+	err := utrans.Import(ut.FormatJSON, "resource/translations")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = utrans.VerifyTranslations()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r := gin.Default()
 	r.Static("/static", "resource/static")
 	r.LoadHTMLGlob("resource/template/**/*")
+	r.Use(func(c *gin.Context) {
+		lang := c.Query("locale")
+		if !isValidatorLanguage(lang) {
+			lang, _ = c.Cookie("locale")
+			if !isValidatorLanguage(lang) {
+				if strings.Contains(c.Request.Header.Get("Accept-Language"), "zh") {
+					lang = mailtrack.LZhHansCN
+				} else {
+					lang = mailtrack.LEn
+				}
+			}
+		}
+		tr, _ := utrans.GetTranslator(lang)
+		c.Set(mailtrack.CTranslatorKey, tr)
+	})
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{})
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"T": c.MustGet(mailtrack.CTranslatorKey).(ut.Translator),
+		})
 	})
 	r.Run()
+}
+
+func isValidatorLanguage(lang string) bool {
+	return len(lang) > 0 && (lang == "en" || lang == "zh_Hans_CN")
 }
--- mailtrack.go
+++ mailtrack.go
@@ -1,1 +1,10 @@
 package mailtrack
+
+const (
+	// CTranslatorKey 取翻译实例的键
+	CTranslatorKey = "c_tras_k"
+	// LZhHansCN 大陆普通话
+	LZhHansCN = "zh_Hans_CN"
+	// LEn 英语
+	LEn = "en"
+)
